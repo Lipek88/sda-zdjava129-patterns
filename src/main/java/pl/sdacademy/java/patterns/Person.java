71fbaa43e6b8881c2c1de6fafc468fd52a2d48c6
Maciej Lipinski <maciej.lipinski123@gmail.com>
2022-11-20 12:08:41 +0100

    Person, Student, Teacher, HasPerson

---
+++ src/main/java/pl/sdacademy/java/patterns/Person.java
@@ -0,0 +1,22 @@
+package pl.sdacademy.java.patterns;
+
+import java.util.UUID;
+
+public class Person {
+
+    private UUID uuid;
+    private String firstNAme;
+    private String lastName;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getFirstNAme() {
+        return firstNAme;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
